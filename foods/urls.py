832562f18e75bc78b1805fc0e433cef4af6f2351
alimardon <aboqijonov12@gmail.com>
2024-04-14 19:00:23 +0500

    resto 2

--- foods/urls.py
+++ foods/urls.py
@@ -3,4 +3,6 @@ from .views import *
 urlpatterns = [
     path('', home_view, name='home'),
     path('menu/', menu_view, name='menu'),
+    path('about/', about_view, name='about'),
+    path('menu/<int:pk>/', food_detail_view, name='food_detail'),
 ]
